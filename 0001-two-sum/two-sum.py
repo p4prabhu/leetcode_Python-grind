458f68eb6758b8b76d1f90eb2ed1073d644383d0
Prabhat Kumar Singh <46450896+p4prabhu@users.noreply.github.com>
2025-04-26 18:09:16 -0400

    Time: 0 ms (100.00%) Space: 19.1 MB (24.72%)

--- 0001-two-sum/two-sum.py
+++ 0001-two-sum/two-sum.py
@@ -1,12 +1,26 @@
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         dic={}
+#         for i, j in enumerate(nums):
+#             if target-j in dic:
+#                 return [dic[target-j],i]
+#             else:
+#                 dic[j]=i
+    
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         dic={}
-        for i, j in enumerate(nums):
-            if target-j in dic:
-                return [dic[target-j],i]
+        for i, num in enumerate(nums):
+            if target-num in dic:
+                return [dic[target-num],i]
             else:
-                dic[j]=i
+                dic[num]=i
+
+
+
     
+       
 
 
 
