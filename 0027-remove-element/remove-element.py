62b9b7f6b8d70e0c847dc74639cc0959dd043b4c
Prabhat Kumar Singh <46450896+p4prabhu@users.noreply.github.com>
2025-04-27 01:45:53 -0400

    Time: 0 ms (100.00%) Space: 18 MB (11.29%)

--- 0027-remove-element/remove-element.py
+++ 0027-remove-element/remove-element.py
@@ -1,15 +1,22 @@
+
+
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        k=0
+        j=0
         for i in range(len(nums)):
             if nums[i]!=val:
-                nums[k]=nums[i]
-                k+=1     
-        return k
+                nums[j]=nums[i]
+                j+=1
+        return j
 
 
+
+ 
 
         
+
+
+
 
 
        
