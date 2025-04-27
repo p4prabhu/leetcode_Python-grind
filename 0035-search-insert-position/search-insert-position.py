fe09d706f8e0a63576634ac1764c83b8e545cc73
Prabhat Kumar Singh <46450896+p4prabhu@users.noreply.github.com>
2025-04-27 02:21:12 -0400

    Time: 0 ms (100.00%) Space: 18.2 MB (94.07%)

--- 0035-search-insert-position/search-insert-position.py
+++ 0035-search-insert-position/search-insert-position.py
@@ -1,15 +1,31 @@
+# class Solution:
+#     def searchInsert(self, nums: List[int], target: int) -> int:
+#         l, r=0, len(nums)-1
+#         while l<=r:
+#             mid=(l+r)//2
+#             if target==nums[mid]:
+#                 return mid
+#             if target>nums[mid]:
+#                 l=mid+1
+#             else:
+#                 r=mid-1
+#         return l
+
+
+
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        l, r=0, len(nums)-1
+        l, r= 0, len(nums)-1
         while l<=r:
             mid=(l+r)//2
-            if target==nums[mid]:
+            if nums[mid]==target:
                 return mid
-            if target>nums[mid]:
-                l=mid+1
-            else:
+            if target<nums[mid]:
                 r=mid-1
+            else:
+                l=mid+1
         return l
+
 
 
         
