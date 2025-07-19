d92e85584ebc8993c9e7aaa80f2ba7f228413579
Prabhat Kumar Singh <46450896+p4prabhu@users.noreply.github.com>
2025-07-18 21:46:14 -0400

    Time: 412 ms (98.48%), Space: 0B (100%) - LeetHub

---
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -0,0 +1,27 @@
+# Write your MySQL query statement below
+
+
+-- select name from customer where
+-- ifnull(referee_id,0)!=2
+
+
+
+
+
+
+-- select name from customer
+-- where ifnull(referee_id,0)!=2
+
+
+
+
+
+
+
+
+
+
+select name from Customer
+where ifnull(referee_id,0) !=2
+
+
