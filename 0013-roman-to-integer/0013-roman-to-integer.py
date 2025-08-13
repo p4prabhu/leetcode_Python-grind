fde6aa4cfe60c112c5174108a68bbfbbc9543a7f
Prabhat Kumar Singh <46450896+p4prabhu@users.noreply.github.com>
2025-08-13 01:39:33 -0400

    Time: 4 ms (63.09%), Space: 17.7 MB (64.18%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -10,15 +10,32 @@
 #         return n
 
 
+# class Solution:
+#     def romanToInt(self, s: str) -> int:
+#         h={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+#         res=0
+#         for i in range(len(s)):
+#             if i+1<len(s) and h[s[i]] < h[s[i+1]]:
+#                 res=res-h[s[i]]
+#             else:
+#                 res+=h[s[i]]
+#         return res
+
+
+
 class Solution:
     def romanToInt(self, s: str) -> int:
-        h={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
-        res=0
-        for i in range(len(s)):
-            if i+1<len(s) and h[s[i]] < h[s[i+1]]:
-                res=res-h[s[i]]
+         h={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+         num=0
+         for i in range(len(s)):
+            if i+1<len(s) and h[s[i]]<h[s[i+1]]:
+                num=num-h[s[i]]
             else:
-                res+=h[s[i]]
-        return res
-            
+                num = num + h[s[i]]
+         return num
+
+
+
+        
+                    
 
