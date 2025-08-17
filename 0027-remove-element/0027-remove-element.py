1e5b0f96932fd2b9a890cc6914ddbab4de57df8b
Prabhat Kumar Singh <46450896+p4prabhu@users.noreply.github.com>
2025-08-17 17:32:54 -0400

    Time: 0 ms (100%), Space: 17.7 MB (52.21%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.py
@@ -0,0 +1,40 @@
+
+
+# class Solution:
+#     def removeElement(self, nums: List[int], val: int) -> int:
+#         j=0
+#         for i in range(len(nums)):
+#             if nums[i]!=val:
+#                 nums[j]=nums[i]
+#                 j+=1
+#         return j
+
+
+
+ 
+
+
+
+
+
+
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+
+        out=0
+        for i in range(len(nums)):
+            if nums[i]!=val:
+                nums[out]=nums[i]
+                out+=1
+        return out
+
+
+        
+
+
+
+
+
+       
