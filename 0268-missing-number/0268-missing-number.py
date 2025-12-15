06b260c84ce19c82c8ec5f329fb89ece811567fd
Prabhat Kumar Singh <46450896+p4prabhu@users.noreply.github.com>
2025-12-15 17:20:48 -0500

    Time: 11 ms (25.17%), Space: 18.8 MB (35.24%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,14 +1,26 @@
-class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        nums.sort()
-        for i, v in enumerate(nums):
-            if i!=v:
-                return v-1
-            if v==len(nums)-1:
-                return v+1
+# class Solution:
+#     def missingNumber(self, nums: List[int]) -> int:
+#         nums.sort()
+#         for i, v in enumerate(nums):
+#             if i!=v:
+#                 return v-1
+#             if v==len(nums)-1:
+#                 return v+1
 
 
 
 # class Solution:
 #     def missingNumber(self, nums: List[int]) -> int:
 #         return sum(range(len(nums)+1))- sum(nums)
+
+
+
+
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        for i, v in enumerate(nums):
+            if i != v:
+                return i
+        return len(nums)
